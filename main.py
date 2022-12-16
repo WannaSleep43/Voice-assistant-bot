45399f4983c1230a7d4ac0d6c96cabf14c4c11f0
Kirill <ept-mat@mail.ru>
2022-12-16 12:46:22 +0300

    added speech-to-text

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from aiogram import Bot, types
+from aiogram.types import ContentType
 from aiogram.utils import executor
 from aiogram.dispatcher import Dispatcher
 from random import randint
-import silero, torch, time, torchaudio
-import sqlite3
+import soundfile as sf
+import speech_recognition as sr
+import silero, torch, json, sqlite3, librosa, wave
+from vosk import Model, KaldiRecognizer
 
 
 # bot consts
@@ -28,6 +31,12 @@ joke_cursor = sqlite3.connect("base.db").cursor()
 joke_list = []
 
 
+# speech to text
+rate = 44100
+speech_model = Model('small model')
+rec = KaldiRecognizer(speech_model, rate)
+
+
 def get_joke():
     if len(joke_list):
         res = joke_list[-1]
@@ -42,21 +51,52 @@ def get_joke():
         return get_joke()
 
 
-@dp.message_handler(commands=['tell_joke'])
-async def tell_a_joke(message: types.Message):
-    print(1)
-    audio = model.save_wav(text=get_joke(),
+def make_audio(text):
+    audio = model.save_wav(text=text,
                            speaker=speaker,
                            sample_rate=sample_rate,
                            put_accent=put_accent,
                            put_yo=put_yo)
+    return audio
+
+@dp.message_handler(commands=['tell_joke'])
+async def tell_a_joke(message: types.Message):
+    audio = make_audio(get_joke())
     await message.answer_voice(open(audio, 'rb'))
 
 
 @dp.message_handler(commands=['read_joke'])
 async def read_a_joke(message: types.Message):
-    print(2)
     await message.answer(get_joke())
 
+
+@dp.message_handler(content_types=[ContentType.VOICE])
+async def speech_to_text(message: types.Message):
+    file_id = message.voice.file_id
+    file = await bot.get_file(file_id)
+    path = file.file_path
+    await bot.download_file(path, 'voice.wav')
+    x, _ = librosa.load('voice.wav', sr=rate)
+    sf.write('voice.wav', x, rate)
+    wf = wave.open('tmp.wav', 'rb')
+    result = ""
+    last_n = False
+    while True:
+        data = wf.readframes(rate)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            res = json.loads(rec.Result())
+            if res['text'] != '':
+                result += f" {res['text']}"
+                last_n = False
+            elif not last_n:
+                result += '\n'
+                last_n = True
+    res = json.loads(rec.FinalResult())
+    result += res['text']
+    if result:
+        await message.answer(result)
+        # await message.answer_voice(open(make_audio(result), 'rb'))
 
 executor.start_polling(dp, skip_updates=True)
